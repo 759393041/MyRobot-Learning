4d9a224d6c84347243eea8a61330b73baa018a2c
759393041 <81805379+759393041@users.noreply.github.com>
2023-10-18 14:31:19 +0800

    1

---
+++ 01 python基础/PyCharmCodeSpace/第一课 python/09.变量的数据类型.py
@@ -0,0 +1,15 @@
+"""
+数字型变量
+"""
+# 整型
+NumberInt = 1
+print(type(NumberInt))
+# 浮点数 float
+NumberFloat = 1.414
+print(type(NumberFloat))
+# Bool
+NumberBool = False
+print(type(NumberBool))
+# 复数
+NumberComplex = 1+1j
+print(type(NumberComplex))
