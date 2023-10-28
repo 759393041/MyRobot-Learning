dd70ac67538edff42a2b1c650ac3fbc9098eea56
759393041 <81805379+759393041@users.noreply.github.com>
2023-10-28 12:09:08 +0800

    1

--- 01 python基础/PyCharmCodeSpace/第二课 python基础/019.列表的操作.py
+++ 01 python基础/PyCharmCodeSpace/第二课 python基础/019.列表的操作.py
@@ -1,5 +1,19 @@
+#增
 myl = ['Valorant', 'the Finalls']
 myl.append('csgo')
 print(myl)
 myl.insert(1,'Apex')
 print(myl)
+#删
+myl.pop()
+print(myl)
+del myl[0]
+print(myl)
+myl.remove("Apex")
+print(myl)
+# myl.clear()
+# print(myl)
+"""
+查询
+"""
+
