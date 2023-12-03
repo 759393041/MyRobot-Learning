1d93882df41a775cd7a598ee56b5ad70b34e0f7f
759393041 <81805379+759393041@users.noreply.github.com>
2023-12-03 19:46:53 +0800

    11

---
+++ 02 C++/8-宏,枚举，异常处理，文件IO/code&资料/code/Emun_errorWaring/exptional_use.cpp
@@ -0,0 +1,25 @@
+//#include "exptional_use.h"
+//#include<exception>
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//int main()
+//{
+//	exptional_use aa;
+//	aa.arr.push_back(1);
+//	try
+//	{
+//		aa.test1(aa.arr,0);
+//		cout << "0meishi" << endl;
+//		aa.test1(aa.arr, 1);
+//
+//	}
+//	catch (exception e)
+//	{
+//		cout << "here" << endl;
+//	}
+//
+//
+//	return 0;
+//}
---
+++ 02 C++/8-宏,枚举，异常处理，文件IO/code&资料/code/Emun_errorWaring/exptional_use.h
@@ -0,0 +1,20 @@
+//#pragma once
+//#include<exception>
+//#include<vector>
+//#include<iostream>
+//using namespace std;
+//class exptional_use
+//{
+//public:
+//	vector<int>	arr;
+//	int size;
+//	void test1(vector<int>arr,int size)
+//	{
+//		if (this->size<size)
+//		{
+//			throw  exception("Exception");
+//
+//		}
+//	}
+//};
+//
